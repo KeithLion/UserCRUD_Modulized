ca38ae1b96731e197a2287109bc6bdbabe2ee855
KeithLion <nortonkiara6@gmail.com>
2022-01-16 11:58:39 -0800

    created class methods to update, delete, and display one user

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -2,6 +2,8 @@ from config.mysqlconnection import connectToMySQL
 
 
 class User:
+    db = 'new_schema1'
+
     def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
@@ -20,6 +22,22 @@ class User:
         return users
 
     @classmethod
+    def get_one(cls, data):
+        query = 'select* from users where id = %(id)s;'
+        user_data = connectToMySQL(cls.db).query_db(query, data)
+        return cls(user_data[0])
+
+    @classmethod
     def save(cls, data):
         query = 'INSERT INTO users (first_name, last_name,email,created_at,updated_at) VALUES ( %(fname)s ,  %(lname)s  %(ema)s, NOW(), NOW());'
         return connectToMySQL('new_schema1').query_db(query, data)
+
+    @classmethod
+    def update(cls, data):
+        query = 'update users set first_name = %(fname)s, last_name = %(lname)s,email = %(ema)s where id = %(id)s;'
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        query = ' update users set first_name = %(fname)s, last_name = %(lname)s, email = %(ema)s where id = %(id)s;'
+        return connectToMySQL(cls.db).query_db(query, data)
