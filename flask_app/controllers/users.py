0e42b6abf55d6a83d3671223c4fff85a74066542
KeithLion <nortonkiara6@gmail.com>
2022-01-16 11:43:34 -0800

    created the controller and model files

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,25 +1,24 @@
-from config.mysqlconnection import connectToMySQL
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.user import User
 
 
-class User:
-    def __init__(self, data):
-        self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.email = data['email']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+@app.route('/')
+def create_user():
+    return render_template('form1.html')
 
-    @classmethod
-    def get_all(cls):
-        query = 'select * from users;'
-        results = connectToMySQL('new_schema1').query_db(query)
-        users = []
-        for user in results:
-            users.append(cls(user))
-        return users
 
-    @classmethod
-    def save(cls, data):
-        query = 'INSERT INTO users (first_name, last_name,email,created_at,updated_at) VALUES ( %(fname)s ,  %(lname)s  %(ema)s, NOW(), NOW());'
-        return connectToMySQL('new_schema1').query_db(query, data)
+@app.route('/create_users', methods=['post'])
+def new_user():
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'ema': request.form['ema']
+    }
+    User.save(data)
+    return redirect('/allusers')
+
+
+@app.route('/allusers')
+def all():
+    return render_template('form2.html', users=User.get_all())
