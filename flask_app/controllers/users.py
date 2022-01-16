a272caf9439122da058bc6e093210ddde2612eb2
KeithLion <nortonkiara6@gmail.com>
2022-01-16 12:24:20 -0800

    created routes to update, delete, and create new users

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -8,17 +8,57 @@ def create_user():
     return render_template('form1.html')
 
 
-@app.route('/create_users', methods=['post'])
+@app.route('/create_users', methods=['get', 'post'])
 def new_user():
+    if request.method == 'get':
+        return redirect('/')
     data = {
+        'id': id,
         'fname': request.form['fname'],
         'lname': request.form['lname'],
         'ema': request.form['ema']
     }
     User.save(data)
-    return redirect('/allusers')
+    return redirect('/')
 
 
 @app.route('/allusers')
 def all():
     return render_template('form2.html', users=User.get_all())
+
+
+@app.route('/users/<int:id>')
+def show_one(id):
+    data = {
+        'id': id,
+    }
+    return render_template('one_user.html', user=User.get_one(data))
+
+
+@app.route('/users/<int:id>/update_page')
+def update_page(id):
+    data = {
+        'id': id,
+    }
+    return render_template('edit.html', user=User.get_one(data))
+
+
+@app.route('/users/<int:id>/update', methods=['post'])
+def update_user(id):
+    data = {
+        'id': id,
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'ema': request.form['ema'],
+    }
+    User.update(data)
+    return redirect('/')
+
+
+@app.route('/users/<int:id>/delete', methods=['post'])
+def delete_user(id):
+    data = {
+        'id': id
+    }
+    User.delete(data)
+    return redirect('/')
