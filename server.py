c206d06d843470cf9e74f310865b7ebf307fe9c5
KeithLion <nortonkiara6@gmail.com>
2022-01-16 11:43:19 -0800

    connected the server to the controller

--- server.py
+++ server.py
@@ -1,27 +1,5 @@
-from flask import Flask, render_template, redirect, request
-from models.usersollers.users import User
-app = Flask(__name__)
-
-
-@app.route('/')
-def create_user():
-    return render_template('form1.html')
-
-
-@app.route('/create_users', methods=['post'])
-def new_user():
-    data = {
-        'fname': request.form['fname'],
-        'lname': request.form['lname'],
-        'ema': request.form['ema']
-    }
-    User.save(data)
-    return redirect('/allusers')
-
-
-@app.route('/allusers')
-def all():
-    return render_template('form2.html', users=User.get_all())
+from flask_app import app
+from flask_app.controllers import users
 
 
 if __name__ == "__main__":
